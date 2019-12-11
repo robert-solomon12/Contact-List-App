98fc7a3ff6f8e546620187f585a96d9b0049abc7
Robert Solomon <20079462@mail.wit.ie>
2019-12-11 11:48:39 +0000

    CODE CHANGE

--- src/components/filterControls/index.js
+++ src/components/filterControls/index.js
@@ -2,24 +2,42 @@ import React, { Component } from "react";
 import "./filterControls.css"
 
 export default class FilterControls extends Component {
-  render() {
-    return (
-      <div className="container-fluid">
-        <div className="row bg-warning">
-          <div className="col-md-12">
-            <h4>
-              <span>Filter </span>
-              <input type="text" placeholder="Name Search" />
-              <span> Gender: </span>
-              <select id="gender">
-                <option value="all">All</option>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-              </select>
-            </h4>
-          </div>
+    handleChange = (e, type, value) => {
+        e.preventDefault();
+        this.props.onUserInput(type, value);
+    };
+    handleTextChange = e => {
+        this.handleChange(e, "name", e.target.value);
+    };
+    handleGenderChange = e => {
+        this.handleChange(e, "gender", e.target.value);
+    };
+    render() {
+        return (
+        <div className="container-fluid">
+            <div className="row bg-warning">
+            <div className="col-md-12">
+                <h4>
+                <span>Filter </span>
+                <input
+                    type="text"
+                    placeholder="Name Search"
+                    onChange={this.handleTextChange}
+                />
+                <span> Gender: </span>
+                <select
+                    id="gender"
+                    onChange={this.handleGenderChange}
+                >
+                    <option value="all">All</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                </select>
+                </h4>
+            </div>
+            </div>
         </div>
-      </div>
-    );
-  }
+        );
+    }
+
 }
